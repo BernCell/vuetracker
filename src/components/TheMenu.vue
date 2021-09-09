4828e2c507b73eabe74d79d4dc9ad12d29d4357e
BernCell <bern.cell@hotmail.com>
2021-09-09 05:54:04 +0200

    Ajout d'axios pour la liaison des tâches avec JSONBIN.io(base de donn…

--- src/components/TheMenu.vue
+++ src/components/TheMenu.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
      <el-menu default-active="1">
 
       <h3>Vue Tracker</h3>
@@ -42,44 +41,3 @@
 
 </style>
 
-=======
-  <el-menu default-active="1">
-
-    <h3>Vue Tracker</h3>
-
-    <el-menu-item index="1">
-      <i class="el-icon-time"></i>
-      <span>Tracker</span>
-    </el-menu-item>
-
-    <el-menu-item index="2" disabled>
-      <i class="el-icon-folder-opened"></i>
-      <span>Catégories</span>
-    </el-menu-item>
-
-    <el-menu-item index="3" disabled>
-      <i class="el-icon-setting"></i>
-      <span>Paramètres</span>
-    </el-menu-item>
-    
-  </el-menu>
-</template>
-
-<script>
-  export default {
-    
-  }
-</script>
-
-<style scoped>
-.el-menu {
-  border-right: none !important;
-  text-align: left;
-}
-
-h3 {
-  text-align: center;
-}
-
-</style>
->>>>>>> a9d26aa0242399ce81f396fb9c147be2468cd14d
--- src/components/TheTopTask.vue
+++ src/components/TheTopTask.vue
@@ -1,30 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <el-row>
-
-        <el-col :xs="12" :span="15" :lg="18">
-             <el-input placeholder="Nom de votre tâche" v-model="taskname"></el-input>
-        </el-col>
-
-        <el-col :xs="12" :span="9" :lg="6" class="actions">
-            <el-button type="primary" icon="el-icon-video-play" circle></el-button>
-            <el-button type="danger" icon="el-icon-video-pause" circle></el-button>
-            <span> 00:00:00</span>
-        </el-col>
-
-    </el-row>
-</template>
-
-<script>
-    export default {
-        data() {
-            return {
-                taskname: ""
-            }
-        },
-        
-    }
-=======
   <el-row>
 
     <el-col :xs="12" :span="15" :lg="18">
@@ -138,23 +112,10 @@
       }
     }
   }
->>>>>>> a9d26aa0242399ce81f396fb9c147be2468cd14d
 </script>
 
 <style lang="scss" scoped>
 .el-input {
-<<<<<<< HEAD
-    padding-left: 20px;
-    box-sizing: border-box;
-}
-
-.actions {
-    text-align: right;
-    padding-right: 20px;
-    span {
-        scroll-padding-left: 20px;
-    }
-=======
   padding-left: 20px;
   box-sizing: border-box;
 }
@@ -165,6 +126,5 @@
   strong {
     padding-right: 20px;
   }
->>>>>>> a9d26aa0242399ce81f396fb9c147be2468cd14d
 }
 </style>
