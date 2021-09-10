5f44da8dba7b7fac36cfeaafda54ee415092fbb3
BernCell <bern.cell@hotmail.com>
2021-09-10 21:32:00 +0200

    S2-start Manipulation de components

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,47 @@
 <template>
-<el-container>
+<el-container class="mainContainer" >
 
   <el-aside width="200px">
     <TheMenu/>
   </el-aside>
 
   <el-container>
-    <el-header height="60px" >
-      
-      <TheTopTask />
-       
+
+    <el-header height="60px" >      
+      <TheTopTask />        
     </el-header>
 
     <el-main>
-      <el-table :data="tableData">
-        <el-table-column prop="date" label="Date" width="140">
-        </el-table-column>
-        <el-table-column prop="name" label="Name" width="120">
-        </el-table-column>
-        <el-table-column prop="address" label="Address">
-        </el-table-column>
-      </el-table>
+      <TaskList :tasks="tasks" />
     </el-main>
+
   </el-container>
+
 </el-container>
 </template>
 
 
-
+ 
 
 <script>
 import TheMenu from "./components/TheMenu.vue"
 import TheTopTask from "./components/TheTopTask.vue"
+import TaskList from "./components/TaskList.vue"
+
   export default {
     components: { 
       TheMenu, 
-      TheTopTask },
+      TheTopTask,
+      TaskList
+      },
     data() {
-      const item = {
-        date: '2016-05-02',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
+      const sample = {
+        name: 'Développement de la feature "edit" d\'un tweet',
+        startTime: '01/03/2021',
+        endTime: '00:43:13',
       };
       return {
-        tableData: Array(20).fill(item)
+        tasks: Array(20).fill(sample)
       }
     }
   };
@@ -56,6 +54,11 @@ mais de préférence le 'style scoped' en component.vue  */
 body {margin:0}
 
 #app {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -64,7 +67,10 @@ body {margin:0}
   
 }
 
+.mainContainer { height: 100%; }
+
 .el-aside {
+    height: 100%;
     color: #333; 
     /* color:var(--el-text-color-primary); */
     /* border-right: solid 1px var(--el-menu-border-color) */
